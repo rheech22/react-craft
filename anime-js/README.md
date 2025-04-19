d8173469d1ef68a6c2e85c6afcc4ff4c94bf5652
Aiden <rheech22@gmail.com>
2025-04-20 00:39:49 +0900

    add react-anime

---
+++ anime-js/README.md
@@ -0,0 +1,23 @@
+# 🧩 Create a useDebounce
+
+## 📋 Task
+
+Implement a custom hook useDebounce that delays updating a value until after a specified wait time has passed since the last change.
+
+## ✅ Requirements
+
+- The hook signature should be:
+
+  ```ts
+  function useDebounce<T>(value: T, delay: number): T;
+  ```
+
+- It should return the debounced version of value after the delay.
+- Any change to value should reset the timer.
+- Create a SearchInput component that:
+- Displays both the current value and the debounced value.
+- Logs the debounced value to the console when it updates.
+
+## 🔥 Link
+
+- [Click to see the implementation.](./src/App.tsx)
---
+++ anime-js/src/App.tsx
@@ -0,0 +1,65 @@
+import {
+  animate,
+  createScope,
+  createSpring,
+  createDraggable,
+  Scope,
+} from "animejs";
+import { useEffect, useRef, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const root = useRef(null);
+  const scope = useRef<Scope | null>(null);
+  const [rotations, setRotations] = useState(0);
+
+  useEffect(() => {
+    scope.current = createScope({ root }).add((scope) => {
+      animate(".logo", {
+        scale: [
+          { to: 2, ease: "inOut(3)", duration: 200 },
+          { to: 1, ease: createSpring({ stiffness: 300 }) },
+        ],
+        loop: true,
+        loopDelay: 250,
+      });
+
+      createDraggable(".logo", {
+        container: [0, 0, 0, 0],
+        releaseEase: createSpring({ stiffness: 200 }),
+      });
+
+      scope.add("rotateLogo", (i) => {
+        animate(".logo", {
+          rotate: i * 360,
+          ease: "out(4)",
+          duration: 1500,
+        });
+      });
+    });
+
+    return () => scope.current?.revert();
+  }, []);
+
+  const handleClick = () => {
+    const i = rotations + 1;
+    setRotations(i);
+    scope.current?.methods.rotateLogo(i);
+  };
+
+  return (
+    <div ref={root}>
+      <div>
+        <img src={reactLogo} className="logo" alt="React logo" />
+      </div>
+      <div>
+        <fieldset>
+          <button onClick={handleClick}>rotations: {rotations}</button>
+        </fieldset>
+      </div>
+    </div>
+  );
+}
+
+export default App;
