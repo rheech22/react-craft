45d17b2b5e3b60e7484dc11c2e05a16fab34001d
Aiden <rheech22@gmail.com>
2025-04-22 04:23:08 +0900

    shuffle grid

--- anime-js/src/pages/shuffle.tsx
+++ anime-js/src/pages/shuffle.tsx
@@ -11,12 +11,13 @@ export const Shuffle = () => {
     const squares = utils.$(".square");
 
     scope.current = createScope({ root }).add((scope) => {
-      const x = stagger("3.2rem");
+      const x = stagger("100%", { grid: [6, 2], axis: "x" });
+      const y = stagger("100%", { grid: [6, 2], axis: "y" });
 
-      utils.set(squares, { x });
+      utils.set(squares, { x, y });
 
       scope.add("shuffle", () => {
-        animate(utils.shuffle(squares), { x });
+        animate(utils.shuffle(squares), { x, y });
       });
     });
 
@@ -36,8 +37,46 @@ export const Shuffle = () => {
         <div className="square">D</div>
         <div className="square">E</div>
         <div className="square">F</div>
+        <div className="square">G</div>
+        <div className="square">H</div>
+        <div className="square">I</div>
+        <div className="square">J</div>
+        <div className="square">K</div>
+        <div className="square">L</div>
       </div>
       <button onClick={handleClick}>Shuffle</button>
     </div>
   );
 };
+
+//useEffect(() => {
+//  const squares = utils.$(".square");
+//
+//  const cols = 6;
+//  const squareSize = 48;
+//  const gap = 5;
+//  const totalSquareSize = squareSize + gap;
+//  const getX = (i: number): number => {
+//    return (i % cols) * totalSquareSize;
+//  };
+//
+//  const getY = (i: number): number => {
+//    return Math.floor(i / cols) * totalSquareSize;
+//  };
+//
+//  scope.current = createScope({ root }).add((scope) => {
+//    utils.set(squares, {
+//      x: (_: unknown, i: number) => getX(i),
+//      y: (_: unknown, i: number) => getY(i),
+//    });
+//
+//    scope.add("shuffle", () => {
+//      animate(utils.shuffle(squares), {
+//        x: (_: unknown, i: number) => getX(i),
+//        y: (_: unknown, i: number) => getY(i),
+//      });
+//    });
+//  });
+//
+//  return () => scope.current?.revert();
+//}, []);
